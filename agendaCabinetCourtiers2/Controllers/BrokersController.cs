751fdde0890f24d1245051c4a53eda125d7e1cab
Florent G <florentgleizes@hotmail.com>
2021-04-10 20:00:59 +0200

    ajout, affichage liste et détails, modification et suppression courtiers et client. Controller et view brokers et customers

---
+++ agendaCabinetCourtiers2/Controllers/BrokersController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using agendaCabinetCourtiers2.Models;
+
+namespace agendaCabinetCourtiers2.Controllers
+{
+    public class BrokersController : Controller
+    {
+        private agendaEntities db = new agendaEntities();
+
+        [ActionName("ListBrokers")]
+        public ActionResult Index()
+        {
+            IEnumerable<brokers> brokers = db.brokers.ToList();
+            return View(brokers);
+
+            /*return View(db.brokers.ToList());*/
+        }
+
+        // GET: AddBroker
+        public ActionResult AddBroker()
+        {
+            return View();
+        }
+
+        // POST: AddBroker
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddBroker([Bind(Include = "idBroker,lastname,firstname,mail,phoneNumber")] brokers broker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.brokers.Add(broker);
+                db.SaveChanges();
+                TempData["messageAddBroker"] = "Courtier ajouté";
+                return RedirectToAction("ListBrokers");
+            }
+
+            return View(broker);
+        }
+
+        // Détails d'un Broker
+        // GET: Brokers/ProfilBroker/5
+        public ActionResult ProfilBroker( int? Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brokers broker = db.brokers.Find(Id);
+            if (broker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(broker);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBroker(brokers broker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(broker).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["messageEditBroker"] = "Courtier modifié";
+                TempData["errorForm"] = 0;
+                return RedirectToAction("ProfilBroker/" + broker.idBroker);
+            }
+            TempData["errorForm"] = 1;
+            return View("ProfilBroker", broker);
+        }
+
+        public ActionResult DeleteBroker(int Id)
+        {
+            brokers broker = db.brokers.Find(Id);
+            db.brokers.Remove(broker);
+            db.SaveChanges();
+            TempData["messageDeleteBroker"] = "Courtier supprimé";
+            return RedirectToAction("ListBrokers");
+        }
+    }
+}
---
+++ agendaCabinetCourtiers2/Controllers/CustomersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using agendaCabinetCourtiers2.Models;
+
+namespace agendaCabinetCourtiers2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private agendaEntities db = new agendaEntities();
+
+        // GET: Customers
+        [ActionName("ListCustomers")]
+        public ActionResult Index()
+        {
+            IEnumerable<customers> customers = db.customers.SqlQuery("SELECT iDcustomers,lastname,firstname,mail,phoneNumber,budget FROM customers ORDER BY lastname ASC").ToList();
+            return View(customers);
+        }
+
+        // Action Create
+        // GET: Customers/AddCustomer
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        // Action Create
+        // POST: Customers/AddCustomer
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCustomer([Bind(Include = "idCustomers,lastname,firstname,mail,phoneNumber,budget")] customers customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(customer);
+                db.SaveChanges();
+                TempData["messageAddCustomer"] = "Client ajouté";
+                return RedirectToAction("ListCustomers");
+            }
+            TempData["defaultBudget"] = customer.budget;
+            return View(customer);
+        }
+
+        // Action Details
+        // GET: Customers/ProfilCustomer/5
+        public ActionResult ProfilCustomer(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customers customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/EditCustomer/5
+        /*public ActionResult EditCustomer(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customers customers = db.customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }*/
+
+        // action Edit
+        // POST: Customers/EditCustomer/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditCustomer([Bind(Include = "idCustomers,lastname,firstname,mail,phoneNumber,budget")] customers customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["messageEditCustomer"] = "Client modifié";
+                TempData["errorForm"] = 0;
+                return RedirectToAction("ProfilCustomer/" + customer.idCustomers);
+            }
+            TempData["defaultBudget"] = customer.budget;
+            TempData["errorForm"] = 1;
+            return View("ProfilCustomer", customer);
+        }
+
+        // action Delete
+        // GET: Customers/DeleteCustomer/5
+        public ActionResult DeleteCustomer(int id)
+        {
+            customers customers = db.customers.Find(id);
+            db.customers.Remove(customers);
+            db.SaveChanges();
+            TempData["messageDeleteCustomer"] = "Client supprimé";
+            return RedirectToAction("ListCustomers");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
